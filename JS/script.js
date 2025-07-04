b696a2af9bcce49b91356f18c95c6aa3d831411d
kayleegracia <kayleegracia20@gmail.com>
2025-07-04 08:27:55 +0000

    add.

--- JS/script.js
+++ JS/script.js
@@ -4,3 +4,18 @@ window.onload = function() {
   document.getElementById('welcomeMessage').innerText = `Welcome to Kaylee Website`;
 };
 
+function validationForm(){
+    const nameInput = document.getElementById('name-Input');
+    if(nameInput.valuee ===''){
+        alert('Please enter yout name!');
+    }else{
+        document.getElementById('result-form').innerHTML = nameInput.value;
+    }
+}
+
+  const form = document.getElementById('contactForm');
+  form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Lakukan aksi kirim data di sini...
+    document.getElementById('thankYouMessage').classList.remove('hidden');
+  });
