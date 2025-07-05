fcbaa2f07cbb04a45378b22a3cd260d175560dd7
kayleegracia <kayleegracia20@gmail.com>
2025-07-05 03:20:01 +0000

    add.

--- JS/script.js
+++ JS/script.js
@@ -10,6 +10,9 @@ function validateForm(event) {
   const first = document.getElementById("firstName").value.trim();
   const last = document.getElementById("lastName").value.trim();
   const email = document.getElementById("email").value.trim();
+  const birthDate = document.getElementById("birthDate").value.trim();
+  const gender = document.qetElementById('input[name="gender"]:checked');
+  const message = document.queryElementById("message").value.trim();
 
   const nameRegex = /^[A-Za-z\s]+$/;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
@@ -24,8 +27,20 @@ function validateForm(event) {
     return false;
   }
 
-    document.querySelector("form").classList.add("hidden");
-    document.getElementById("thankYouPage").classList.remove("hidden");
+  if (!birthDate) {
+    alert("Please enter your date of birth.");
+    return false;
+  }
+
+  if (!gender) {
+    alert("Please select your gender.");
+    return false;
+  }
+
+   if (!emailRegex.test(message)) {
+    alert("Please enter your message.");
+    return false;
+  }
 
   // Kalau valid, pindah ke halaman "Thank You"
   window.location.href = "thankyou.html";
